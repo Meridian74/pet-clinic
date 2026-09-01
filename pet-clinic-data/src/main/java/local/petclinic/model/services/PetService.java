be0ca88fbae3194e8023c53101c1ad51e3fe0c95
meridian74 <meridian@freemail.hu>
2026-09-01 22:34:18 +0200

    Implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/local/petclinic/model/services/PetService.java
@@ -0,0 +1,13 @@
+package local.petclinic.model.services;
+
+import local.petclinic.model.Pet;
+import java.util.Set;
+
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/local/petclinic/model/services/VetService.java
@@ -0,0 +1,12 @@
+package local.petclinic.model.services;
+
+import local.petclinic.model.Vet;
+import java.util.Set;
+
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Set<Vet> findAll();
+    Vet save(Vet vet);
+}
