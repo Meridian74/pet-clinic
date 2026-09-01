939d94569c1bb10aa1f8e2fb299964b7cfd1ea04
meridian74 <meridian@freemail.hu>
2026-09-01 15:43:31 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/local/petclinic/model/Person.java
@@ -0,0 +1,19 @@
+package local.petclinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+
+}
---
+++ src/main/java/local/petclinic/model/Vet.java
@@ -0,0 +1,16 @@
+package local.petclinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vet extends Person {
+
+
+}
