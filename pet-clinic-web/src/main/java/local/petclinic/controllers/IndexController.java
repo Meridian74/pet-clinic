90562259534ce4b8ebff4111c4d7932ab47ab237
meridian74 <meridian@freemail.hu>
2026-09-03 19:40:54 +0200

    Adding index controller and page file, closes #6.

---
+++ pet-clinic-web/src/main/java/local/petclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package local.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
