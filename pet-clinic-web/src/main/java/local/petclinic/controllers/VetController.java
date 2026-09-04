560da61e0ffbe2dbb566150f7dad7a907e162ffe
meridian74 <meridian@freemail.hu>
2026-09-04 23:19:17 +0200

    Added Vets List and updated controller, closes #15.

--- pet-clinic-web/src/main/java/local/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/local/petclinic/controllers/VetController.java
@@ -1,5 +1,7 @@
 package local.petclinic.controllers;
 
+import local.petclinic.services.VetService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
+@AllArgsConstructor
 public class VetController {
+
+    private final VetService vetService;
 
     @GetMapping({"", "/", "/index", "/index.html"})
     public String listOfVets(Model model) {
-
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
+
 }
